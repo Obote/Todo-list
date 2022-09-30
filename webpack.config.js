b291bb1158f9f9aeeb3549d0f362668e8e88e16f
Obote <obotedenis256@gmail.com>
2022-09-30 17:17:45 +0300

    Update webserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-    mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
+    watchFiles: ['src/*.html'],
+    hot: true,
     static: './dist',
   },
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   module: {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
- };
+};
