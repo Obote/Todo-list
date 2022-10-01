272fd8999bd22a61729e58b7c73d89364db3b4cd
Obote <obotedenis256@gmail.com>
2022-10-01 19:40:03 +0300

    Implement reviewer feedback

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import './style.css';
-import List from './list.js';
+import createTodo from './modules/renderTask.js';
+import add from './modules/addTask.js';
+import status from './modules/clearAll.js';
 
-const todoList = new List();
-todoList.display();
-// This code adds a new activity
-document.querySelector('#add-task').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const activity = e.target.elements.activity.value;
-  todoList.addActivity(activity);
-  e.target.reset();
-});
-
-// This code clears activity
-document.querySelector('.clear-completed').addEventListener('click', () => {
-  todoList.clearCompleted();
-});
+window.onload = createTodo();
+add();
+status();
