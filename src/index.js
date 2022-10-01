a8da8836787e2d17e5a5916e4b427e4e724dae81
Obote <obotedenis256@gmail.com>
2022-10-01 15:38:39 +0300

    Add interactive feature

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import List from './list.js';
 
 const todoList = new List();
 todoList.display();
-// This line of code adds new Activity
+// This code adds a new activity
 document.querySelector('#add-task').addEventListener('submit', (e) => {
   e.preventDefault();
   const activity = e.target.elements.activity.value;
@@ -11,12 +11,7 @@ document.querySelector('#add-task').addEventListener('submit', (e) => {
   e.target.reset();
 });
 
-// This line of clears completed Activities
+// This code clears activity
 document.querySelector('.clear-completed').addEventListener('click', () => {
   todoList.clearCompleted();
-});
-
-// This line of code clears all handler
-document.querySelector('#delete-all').addEventListener('click', () => {
-  todoList.clearAll();
 });
